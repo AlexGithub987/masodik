5ddc136be8737828e4b66cf157626469925512d0
Medioworks <sandorkolozs@medioworks.com>
2024-02-06 10:51:10 +0100

    e

--- src/Masodik.php
+++ src/Masodik.php
@@ -4,4 +4,15 @@ namespace Alex\Masodik;
 
 class Masodik
 {
+
+    public static function getName()
+    {
+        return 'hello';
+    }
+
+    public static function getVersion()
+    {
+        return 'v1';
+    }
+    
 }
